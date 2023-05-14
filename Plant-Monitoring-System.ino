6668c5b5a5d5affd7242b691c443fd7a83e96302
Juan Jonathan <juan.jonathan@ui.ac.id>
2023-05-14 10:55:20 +0700

    Updated code and proteus simulation

--- Plant-Monitoring-System.ino
+++ Plant-Monitoring-System.ino
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* Notes
 Output :
 LED kering : Pin Digital 1 (PD1)
@@ -13,16 +12,3 @@ DHT : Pin Digital 7 (PD7)
 MAX7219 : SPI
 
 */
-=======
-/*
-Notes :
-R24 : temp
-R23 : humid
-R22 : moist
-R21 & R20 --> BCD
-R21 : right digit
-R20 : left digit
-R25 - Low Byte Output YL39
-R26 - High Byte Output YL39
-*/
->>>>>>> 126c4eaee94dc1116aa794b9fbf827bfc74caf19
